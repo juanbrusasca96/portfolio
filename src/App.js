35bbbc694fbf6771cd4d6acc31f389491ea0823e
Juan Brusasca <juanbrusasca96@gmail.com>
2022-10-21 09:06:52 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import './style.css';
+import "animate.css/animate.min.css";
+import Navbar from './components/navbar/Navbar';
+import AboutMe from './components/aboutMe/AboutMe';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutMe />
+      <Navbar />
+      <Skills />
     </div>
   );
 }
