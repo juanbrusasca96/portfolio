eb64608b6d4a7c8c030a53cded55f2b76af4f25c
Juan Brusasca <juanbrusasca96@gmail.com>
2022-10-25 15:11:03 -0300

    contact components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/navbar/Navbar';
 import AboutMe from './components/aboutMe/AboutMe';
 import Skills from './components/skills/Skills';
 import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
